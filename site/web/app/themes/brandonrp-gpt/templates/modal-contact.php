a1aa11e7866825708a56f55b90e0322bd73905c4
Brandon Pirruccello <brandon@sectionstudios.com>
2025-11-11 22:57:05 -0800

    Contact form styles in a good place.

--- site/web/app/themes/brandonrp-gpt/templates/modal-contact.php
+++ site/web/app/themes/brandonrp-gpt/templates/modal-contact.php
@@ -1,6 +1,6 @@
 <div class="modal-wrapper">
   <div class="modal-content">
-    <span class="modal-close">q</span>
-    <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]'); ?>
+    <span class="modal-close"></span>
+    <?php echo do_shortcode('[contact-form-7 id="9cde353" title="Contact form 1"]'); ?>
   </div>
 </div>
